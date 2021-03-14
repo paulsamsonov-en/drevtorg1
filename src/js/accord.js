724dc2fd694a462ba7e13a07dfec873b56cf00b7
Paul Samson <sturmann333@gmail.com>
2021-03-14 19:58:43 +0300

    add page zakaz

--- src/js/accord.js
+++ src/js/accord.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-    console.log($(".quest_wrap_questions"));
+
     $(".quest_wrap_questions").click(function() {
         $(this).next(".quest_wrap_answer").toggle();
+        $(this).find(".vert").toggle();
     });
 });
